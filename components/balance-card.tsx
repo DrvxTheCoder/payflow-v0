e3e4650561a8be864a93476af76a7aa5b2139864
DrvxTheCoder <princeflan741@gmail.com>
2026-07-26 00:04:14 +0000

    fix:Further ui tweaks to the Balance Card component

--- components/balance-card.tsx
+++ components/balance-card.tsx
@@ -71,27 +71,29 @@ export function BalanceCard() {
       </div>
 
 
-        <div className="w-full flex flex-row gap-0 justify-center">
+        <div className="w-full flex flex-row gap-2 justify-center items-center">
           <Button
-            className="py-6 bg-card hover:bg-secondary text-primary flex flex-row items-center justify-between rounded-none rounded-l-full border border-muted-foreground/10 text-sm font-medium transition-colors">
-              <HugeiconsIcon icon={CoinsDollarIcon} className="size-6 text-muted-foreground/50" />
+            className="py-6 bg-card hover:bg-card/80 text-primary flex flex-row items-center justify-between rounded-full border border-muted-foreground/10 text-sm font-medium transition-colors">
+              <HugeiconsIcon icon={CoinsDollarIcon} className="size-4 md:size-6 text-primary" />
               Top Up
               <span />
-          </Button> 
-          <Button
-            className="py-6 bg-card hover:bg-secondary text-primary flex flex-row items-center justify-between rounded-none border border-muted-foreground/10 text-sm font-medium transition-colors">
-              <HugeiconsIcon icon={CircleArrowUpRightIcon} className="size-6 text-muted-foreground/50" />
-              Send
-              <span />
-          </Button> 
-
-          <Button
-            className="py-6 bg-card hover:bg-secondary text-primary flex flex-row items-center justify-between rounded-none rounded-r-full border border-muted-foreground/10 border-l-0 text-sm font-medium transition-colors"
-          >
-            <HugeiconsIcon icon={CircleArrowDownLeftIcon} className="size-6 text-muted-foreground/50" />
-            Request
-            <span />
           </Button>
+          <div className="flex flex-row gap-0 justify-center">
+            <Button
+              className="py-6 bg-secondary hover:bg-muted-foreground/10 text-primary flex flex-row items-center justify-between rounded-none rounded-l-full border border-muted-foreground/10 text-sm font-medium transition-colors">
+                <HugeiconsIcon icon={CircleArrowUpRightIcon} className="size-4 md:size-6 text-muted-foreground/50" />
+                Send
+                <span />
+            </Button> 
+
+            <Button
+              className="py-6 bg-secondary hover:bg-muted-foreground/10 text-primary flex flex-row items-center justify-between rounded-none rounded-r-full border border-muted-foreground/10 border-l-0 text-sm font-medium transition-colors"
+            >
+              <HugeiconsIcon icon={CircleArrowDownLeftIcon} className="size-4 md:size-6 text-muted-foreground/50" />
+              Request
+              <span />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
